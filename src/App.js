2ff4b0417e4125fc852b6156c56560b6a6800e2c
Pavel <pn9-5@yandex.ru>
2023-04-17 12:49:52 +0300

    feat: basic main page created Mon, Apr 17, 2023 12:49:52 PM

--- src/App.js
+++ src/App.js
@@ -1,26 +1,49 @@
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-        <div className="headerLeft">
-        <img width={40} height={40} src="/images/logo-1.png" />
+    <div className="wrapper clear">
+      <header className="header d-flex justify-between align-center p-40">
+        <div className="headerLeft d-flex align-center">
+          <img width={40} height={40} src="/images/logo-1.png" />
           <div className="headerInfo">
-            <h3>React Sneakers</h3>
-            <p>Магазин лучших кроссовок</p>
+            <h3 className="text-uppercase">React Sneakers</h3>
+            <p className="opacity-5">Магазин лучших кроссовок</p>
           </div>
         </div>
-          <ul className="headerRight">
-            <li>
-              <svg />
-              <span>1205 руб.</span>
-            </li>
-            <li>
-              <svg />
-            </li>
-          </ul>
+        <ul className="headerRight d-flex">
+          <li className="mr-20">
+            <img width={18} height={18} src="/images/cart.svg" />
+            <span>1205 руб.</span>
+          </li>
+          <li className="mr-10">
+            <img width={18} height={18} src="/images/heart.svg" />
+          </li>
+          <li className="mr-10">
+            <img width={18} height={18} src="/images/user.svg" />
+          </li>
+        </ul>
       </header>
-      <section className="content">
-        <h1>Все кроссовки</h1>
+      <section className="content p-40">
+        <h1 className="mb-40">Все кроссовки</h1>
+        <div className="d-flex">
+          <div className="card">
+            <img
+              className="cardImg"
+              width={133}
+              height={112}
+              src="/images/sneakers/1.png"
+              alt="img"></img>
+            <h4>Мужские Кроссовки Nike Blazer Mid Suede</h4>
+            <div className="d-flex justify-between align-center">
+              <div className="d-flex flex-column">
+                <span>Цена: </span>
+                <b>12 999 руб.</b>
+              </div>
+              <button className="button">
+                <img width={11} height={11} src="/images/plus.svg" alt="plus"></img>
+              </button>
+            </div>
+          </div>
+        </div>
       </section>
     </div>
   );
