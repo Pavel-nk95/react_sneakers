df987c0d92d857c34fa69eb61291c3d99759563b
Pavel <pn9-5@yandex.ru>
2023-04-19 17:50:34 +0300

    feat: cart drawer layout is done. Wed, Apr 19, 2023  5:50:34 PM

--- src/App.js
+++ src/App.js
@@ -5,14 +5,27 @@ function App() {
         <div className="drawer">
           <h2>Корзина</h2>
           <ul className="cart-items mt-20">
-            <li className="cart-item d-flex align-center">
+            <li className="cart-item d-flex align-center p-10 mb-20">
+              <div
+                style={{ backgroundImage: 'url(/images/sneakers/1.png)' }}
+                className="cartItemImg"></div>
+              <div className="mr-20">
+                <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
+                <b>12 999 руб.</b>
+              </div>
               <img
-                className="mr-20"
-                width={70}
-                height={70}
-                src="/images/sneakers/1.png"
-                alt="img"
+                className="btn-remove"
+                width={32}
+                height={32}
+                src="/images/btn-remove.svg"
+                alt="remove"
               />
+            </li>
+
+            <li className="cart-item d-flex align-center p-10 mb-20">
+              <div
+                style={{ backgroundImage: 'url(/images/sneakers/2.png)' }}
+                className="cartItemImg"></div>
               <div className="mr-20">
                 <p className="mb-5">Мужские Кроссовки Nike Blazer Mid Suede</p>
                 <b>12 999 руб.</b>
@@ -26,6 +39,21 @@ function App() {
               />
             </li>
           </ul>
+          <div className="cart-footer">
+            <ul className="cart-total mb-20">
+              <li className="d-flex align-center mb-10">
+                <span>Итого: </span>
+                <div></div>
+                <b>21 498 руб.</b>
+              </li>
+              <li className="d-flex align-center">
+                <span>Налог 5%:</span>
+                <div></div>
+                <b>1074 руб. </b>
+              </li>
+            </ul>
+            <button className="send-btn"> Оформить заказ </button>
+          </div>
         </div>
       </div>
 
